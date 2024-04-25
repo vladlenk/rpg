1ac2556f9fad96353293c062c789eb10c9f18462
vladlenk <166505088+vladlenk@users.noreply.github.com>
2024-04-25 14:21:29 +0300

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public float speed = 1f;
+    float borderPosX;
+    public Animator animator;
+    public float shootInterval = 1f;
+    public float shootTimer = 0f;
+
+    public float speedPerLevel = 0.2f;
+
+    private void Start()
+    {
+        speed += speedPerLevel * LevelController.level;
+
+        borderPosX = Corn.singleton.transform.position.x;
+    }
+
+    private void Update()
+    {
+        if (shootTimer > 0)
+        {
+            shootTimer -= Time.deltaTime;
+        }
+
+        float enemyPosX = transform.position.x;
+
+        if (enemyPosX > borderPosX)
+        {
+            animator.SetBool("isMoving", true);
+            transform.position += -transform.right * speed * Time.deltaTime;
+
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+
+            if (shootTimer <= 0)
+            {
+                Attack();
+                shootTimer = shootInterval;
+            }
+        }
+    }
+
+    public void TakeDamage()
+    {
+        health -= 1;
+
+        if (health <= 0)
+            Destroy(gameObject);
+    }
+
+    public void Attack()
+    {
+        Corn.singleton.TakeDamage();
+    }
+
+}
